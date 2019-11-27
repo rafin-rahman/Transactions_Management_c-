5b723160c21ee4d1eb4356bfe9431b2c2711f7b9
Moshfiqur Rahman Rafin <w1759691@westminster.ac.uk>
2019-11-27 12:54:52 +0000

    just testing

---
+++ enterpriseDevelopment/UserAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enterpriseDevelopment
+{
+    class UserAccount
+    {
+
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string UserPwd { get; set; }
+        public string UserFName { get; set; }
+        
+    }
+}
---
+++ enterpriseDevelopment/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enterpriseDevelopment
+{
+    class UserRepository
+    {
+        public string dbConn;
+        SqlConnection connection;
+        public UserRepository()
+        {
+            dbConn = ConfigurationManager.ConnectionStrings["Conn"].ConnectionString;
+            connection = new SqlConnection(dbConn);
+        }
+
+
+        public bool addUserAccount(UserAccount UserId)
+        {
+            Query = "INSERT INTO UserAccounts([UserId], [UserId]) VALUES(@UserId);";
+
+            return false;
+        }
+    }
+}
